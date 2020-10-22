94d398beecb3dca4b27a96ed327ca9395fb45e41
Anjali Pant <pantanjali7@gmail.com>
2020-10-21 21:06:45 -0400

    created functions to view employees,view wmployees by department,view employees by manager and add employees

--- Employee Tracker.sql
+++ Employee Tracker.sql
@@ -1,24 +1,56 @@
-create database EmployeeTracker;
-use EmployeeTracker;
-create table Department (
-ID int primary key,
-`Name` varchar(30)
+DROP DATABASE IF EXISTS EmployeeTracker;
+
+CREATE DATABASE EmployeeTracker;
+
+USE EmployeeTracker;
+
+CREATE TABLE department (
+	`id` INT PRIMARY KEY auto_increment,
+	`name` VARCHAR(30)
 );
 
-create table `Role` (
-ID int primary key,
-Title varchar(30),
-Salary decimal,
-Department_id int
+CREATE TABLE `role` (
+	`id` INT PRIMARY KEY auto_increment,
+	`title` VARCHAR(30),
+	`salary` DECIMAL,
+	`department_id` INT
 );
 
-create table Employee (
-ID int primary key,
-First_Name varchar(30),
-Last_Name varchar(30),
-Role_id int,
-Manager_id int
+CREATE TABLE employee (
+	`id` INT PRIMARY KEY auto_increment,
+	`first_name` VARCHAR(30),
+	`last_name` VARCHAR(30),
+	`role_id` INT,
+	`manager_id` INT
 );
 
-
-
+INSERT INTO `department` (`id`, `name`) VALUES (1, 'Sales'), (2, 'Engineering'), (3, 'Legal'), (4, 'Finance');
+INSERT INTO `role` (`id`, `title`, `salary`, `department_id`) VALUES 
+	(1, 'Sales Lead', 100000, 1),
+    (2, 'Salesperson', 80000, 1),
+    (3, 'Lead Engineer', 150000, 2),
+    (4, 'Software Enginner', 120000, 2),
+    (5, 'Accountant', 125000, 4),
+    (6, 'Legal Team Lead', 250000, 3),
+    (7, 'Lawyer', 120000, 3);
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `role_id`, `manager_id`) VALUES 
+	(1, 'John', 'Doe', 1, 3),
+    (2, 'Mike', 'Chan', 2, 1),
+    (3, 'Ashley', 'Rodruguez', 3, null),
+    (4, 'Kevin', 'Tupik', 4, 3),
+    (5, 'Malia', 'Brown', 5, null),
+    (6, 'Sarah', 'Lourd', 6, null),
+    (7, 'Tom', 'Allen', 7, 6);
+    
+SELECT 
+	employee.id,
+    employee.first_name,
+    employee.last_name,
+    employeeRole.title,
+	department.name,
+    employeeRole.salary,
+    CONCAT(manager.first_name, ' ', manager.last_name) as manager
+FROM `employee`
+	LEFT JOIN `role` AS employeeRole ON employee.role_id = employeeRole.id 
+	LEFT JOIN `department` AS department ON employeeRole.department_id = department.id 
+	LEFT JOIN `employee` AS manager ON employee.manager_id = manager.id 
--- app.js
+++ app.js
@@ -1,37 +1,204 @@
 const inquirer = require( 'inquirer' );
-const mysql = require( 'mysql' );
-const ctable = require('console.table');
+const Database = require('./database.js');
 
-class Database {
-    constructor( config ) {
-        this.connection = mysql.createConnection( config );
-    }
-    query( sql, args=[] ) {
-        return new Promise( ( resolve, reject ) => {
-            this.connection.query( sql, args, ( err, rows ) => {
-                if ( err )
-                    return reject( err );
-                resolve( rows );
-            } );
-        } );
-    }
-    close() {
-        return new Promise( ( resolve, reject ) => {
-            this.connection.end( err => {
-                if ( err )
-                    return reject( err );
-                resolve();
-            } );
-        } );
-    }
-  }
+const Choices = [
+    "View All Employees",
+    "View All Employees By Department",
+    "View All Employees by Manager",
+    "Add Employees",
+    "Remove Employees",
+    "Update Employee Role",
+    "Update Employee Manager",
+    "Quit"
+];
 
-  const db = new Database({
+function clearConsole() {    
+    process.stdout.write("\u001b[3J\u001b[2J\u001b[1J");
+    console.clear();
+}
+
+async function viewEmployees() {
+    let employees = await db.query(`
+        SELECT 
+            employee.id,
+            employee.first_name,
+            employee.last_name,
+            employeeRole.title,
+            department.name,
+            employeeRole.salary,
+            CONCAT(manager.first_name, ' ', manager.last_name) as manager
+        FROM \`employee\`
+            LEFT JOIN \`role\` AS employeeRole 
+                ON employee.role_id = employeeRole.id 
+            LEFT JOIN \`department\` AS department 
+                ON employeeRole.department_id = department.id 
+            LEFT JOIN \`employee\` AS manager 
+                ON employee.manager_id = manager.id 
+    `);
+    console.table(employees);
+}
+
+async function viewAllEmployeesByDepartment() {
+    let departmentChoice = await db.query("SELECT name FROM department");
+    let departments = departmentChoice.map(itemObj=>itemObj.name);
+    promptAnswers = await inquirer.prompt([
+        { name: 'department', type: 'list', message: "\nSelect the department name to view the employees:",
+        choices: departments }
+    ]);
+    let name = promptAnswers.department;
+    let employees = await db.query(`
+        SELECT 
+            employee.id,
+            employee.first_name,
+            employee.last_name,
+            employeeRole.title,
+            department.name,
+            employeeRole.salary,
+            CONCAT(manager.first_name, ' ', manager.last_name) as manager
+        FROM \`employee\`
+            LEFT JOIN \`role\` AS employeeRole 
+                ON employee.role_id = employeeRole.id 
+            LEFT JOIN \`department\` AS department 
+                ON employeeRole.department_id = department.id 
+            LEFT JOIN \`employee\` AS manager 
+                ON employee.manager_id = manager.id 
+        WHERE department.name="${name}"
+    `);
+    console.table(employees);
+}
+
+async function viewAllEmployeesbyManager() {
+    let managers = await db.query(`
+        SELECT 
+            employee.id,
+            CONCAT(employee.first_name, ' ', employee.last_name) as full_name
+        FROM \`employee\`
+    `);
+
+    let promptAnswers = await inquirer.prompt([ 
+        { name: 'action', type: 'list', message: 'Select a manager?',
+        choices: managers.map(manager => manager.full_name) }
+    ]);  
+
+    let manager;
+    managers.forEach(employee => {
+        if(employee.full_name == promptAnswers.action)
+            manager = employee.id;
+    });
+
+    let employees = await db.query(`
+        SELECT 
+            employee.id,
+            employee.first_name,
+            employee.last_name,
+            employeeRole.title,
+            department.name,
+            employeeRole.salary,
+            CONCAT(manager.first_name, ' ', manager.last_name) as manager
+        FROM \`employee\`
+            LEFT JOIN \`role\` AS employeeRole 
+                ON employee.role_id = employeeRole.id 
+            LEFT JOIN \`department\` AS department 
+                ON employeeRole.department_id = department.id 
+            LEFT JOIN \`employee\` AS manager 
+                ON employee.manager_id = manager.id 
+        WHERE manager.id="${manager}"
+    `);
+
+    if(employees.length == 0) {
+        console.log("This employee is not a manager!");
+    } else
+        console.table(employees);
+}
+
+async function addEmployees() {
+    let titleChoice = await db.query("SELECT id, title FROM role");
+    let titles = titleChoice.map(itemObj=>itemObj.title);
+    let managers = await db.query(`
+    SELECT 
+        employee.id,
+        CONCAT(employee.first_name, ' ', employee.last_name) as full_name
+    FROM \`employee\`
+`);
+
+    promptAnswers = await inquirer.prompt([
+        { name: 'first_name', type: 'input', message: "\nWhat is the employee's first name?" },
+        { name: 'last_name', type: 'input', message: "\nWhat is the employee's last name?" },
+        { name: 'title', type: 'list', message: "\nWhat is the employee's role",choices: titles },
+        { name: 'Manager', type: 'list', message: "\nWho is the employee's manager",choices: managers.map(manager => manager.full_name) }
+    ]);
+
+    let manager;
+    managers.forEach(employee => {
+        if(employee.full_name == promptAnswers.Manager)
+            manager = employee.id;
+    });
+
+    let role;
+    titleChoice.forEach(title => {
+        if(title.title == promptAnswers.title)
+        role = title.id;
+    });
+
+    console.log(role, titleChoice, promptAnswers.title)
+
+    await db.query( "INSERT INTO employee (`first_name`,`last_name`,`role_id`,`manager_id`) VALUES(?,?,?,?)", [promptAnswers.first_name, promptAnswers.last_name, role, manager] );
+}
+
+async function removeEmployees() {
+    let employees = await db.query(`
+    SELECT 
+        employee.id,
+        CONCAT(employee.first_name, ' ', employee.last_name) as full_name
+    FROM \`employee\`
+`);
+promptAnswers = await inquirer.prompt([
+    { name: 'employee', type: 'list', message: "\nWhich employee do you want to remove?",choices: employees.map(employee => employee.full_name) }
+]);
+
+await db.query("DELETE FROM employee WHERE CustomerName='Alfreds Futterkiste';")
+}
+
+async function updateEmployeeRole() {
+    console.log("Update Employee Role");
+}
+
+async function updateEmployeeManager() {
+    console.log("Update Employee Manager");
+}
+
+async function quit() {
+    process.exit();
+}
+
+const actions = {
+    "View All Employees": viewEmployees,
+    "View All Employees By Department": viewAllEmployeesByDepartment,
+    "View All Employees by Manager": viewAllEmployeesbyManager,
+    "Add Employees": addEmployees,
+    "Remove Employees": removeEmployees,
+    "Update Employee Role": updateEmployeeRole,
+    "Update Employee Manager": updateEmployeeManager,
+    "Quit": quit
+};
+
+async function main(){
+    let promptAnswers = await inquirer.prompt([ 
+        { name: 'action', type: 'list', message: 'What do you want to do?',
+        choices: Choices }
+    ]);  
+    clearConsole();
+    let userChoice = promptAnswers.action;
+    await actions[userChoice]();
+    main();
+}
+
+const db = new Database({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "anjalipant123",
     database: "EmployeeTracker"
-  }); 
+}); 
 
-  
+main();
